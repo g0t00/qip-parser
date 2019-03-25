42df9966d9486af2f74a0df426e6162cd4f1a863
Anton Schulte <mail@anton-schulte.de>
2019-03-25 16:27:34 +0100

    added package ordering

--- qip-parser.js
+++ qip-parser.js
@@ -4,17 +4,26 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const {argv} = require('process');
-
+let recursionCounter = 0;
 class QipParser {
-  constructor(path, relativePath) {
+  constructor(path, relativePath, parent) {
+    recursionCounter++;
+    if (recursionCounter > 1000) {
+      throw new Error(`Opening more than 1000 qip-files. Circular Dependency probable. file: ${this.path} referenced in ${this.parent.path}!`);
+    }
     this.path = path;
     this.relativePath = relativePath;
+    this.parent = parent;
     this.getFileContents();
   }
   getFileContents() {
-    this.QipText = fs.readFileSync(this.path, {
-      encoding: 'utf8'
-    });
+    try {
+      this.QipText = fs.readFileSync(this.path, {
+        encoding: 'utf8'
+      });
+    } catch(err) {
+      throw new Error(`Can not open file ${this.path} referenced in ${this.parent.path}!`);
+    }
   }
   parse() {
     let files = [];
@@ -35,7 +44,7 @@ class QipParser {
             let path = pathMatch[1];
             // let mixedFlag = path.match(/pkg/i) !== null ? '-mixedsvvh' : '';
             let mixedFlag = '-mixedsvvh';
-            const file = {path: this.relativePath + path, work};
+            const file = {path: this.relativePath + path, work, parent: this};
             files.push(file);
             // console.log(command);
           } else {
@@ -45,7 +54,7 @@ class QipParser {
           if (pathMatch !== null) {
             let path = pathMatch[1];
             let pathArg = path.replace(/[^/]*$/, '');
-            const subParser = new QipParser(this.relativePath + path, this.relativePath + pathArg);
+            const subParser = new QipParser(this.relativePath + path, this.relativePath + pathArg, this);
             const filesNew = subParser.parse();
             files = files.concat(filesNew);
           } else {
@@ -66,9 +75,13 @@ let relativePath = targetFile.replace(/[^/]+$/, '');
 const touchFile = relativePath + 'sources.touch';
 const parser = new QipParser(targetFile, relativePath);
 let files = parser.parse();
-files.forEach(file => {
-  file.path = file.path.replace(/[^/ .]+\/\.\.\//g, '')
-});
+for (const file of files) {
+  let changedPath = file.path.replace(/[^\/ .]+\/\.\.\//g, '');
+  while (file.path !== changedPath) {
+    file.path = changedPath;
+    changedPath = file.path.replace(/[^\/ .]+\/\.\.\//g, '');
+  }
+}
 let lastCompile;
 try {
   lastCompile = fs.statSync(touchFile).mtimeMs;
@@ -79,10 +92,18 @@ try {
 let doFile = [];
 // order files
 const compare = (a, b) => {
-  // console.log(compare, a, b,  a.path, b.path);
-  if (a.text.match(/^\s*package\s+\w+\s+is/im) !== null) {
-    return false;
-  } else if (b.text.match(/^\s*package\s+\w+\s+is/im) !== null) {
+  // console.log('compare', a.packageName, b.packageName,  a.path, b.path);
+  if (a.isPackage) {
+    if (!b.isPackage) {
+      return false;
+    }
+    // console.log(`use\\s+(${a.work}|work)\\.${a.packageName}\\.all;`, b.path);
+    if (a.text.match(new RegExp(`use\\s+(${b.work}|work)\\.${b.packageName}\\.all;`, 'im')) !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (b.isPackage) {
     return true;
   }
   if (!b.entity) {
@@ -94,10 +115,6 @@ const compare = (a, b) => {
   if (a.text.match(new RegExp(`entity\\s+(${b.work}|work)\\.${b.entity}`, 'im'))) {
     return true;
   }
-  if (b.text.match(new RegExp(`entity\\s+(${a.work}|work)\\.${a.entity}`, 'im'))) {
-    return false;
-  }
-
 
   return false;
 }
@@ -107,21 +124,33 @@ files = files.filter((file, index) => {
   .findIndex(fileSearch => fileSearch.path === file.path && fileSearch.work === fileSearch.work) === -1;
 })
 for (const file of files) {
-  file.text = fs.readFileSync(file.path, {encoding: 'utf8'});
+  try {
+    file.text = fs.readFileSync(file.path, {encoding: 'utf8'});
+  } catch (e) {
+    throw new Error(`Can not open file ${file.path} referenced in ${file.parent.path}!`);
+  }
   const match = file.text.match(/^\s*entity\s+(\w+)\s*is/im);
   if (match !== null) {
     file.entity = match[1];
   } else {
     // console.error('no entity', file.path);
   }
+  const packageMatch = file.text.match(/^\s*package\s+(\w+)\s+is/im);
+  file.isPackage = false;
+  if (packageMatch !== null) {
+    file.packageName = packageMatch[1];
+    file.isPackage = true;
+  }
+
 }
+// console.log(files.map(file => [file.path, file.packageName]));
 let changeCounter = 0;
 while (changed) {
   changed = false;
   loop: for (let a = 0; a < files.length - 1; a++) {
     for (let b = a + 1; b < files.length; b++) {
       if (compare(files[a], files[b])) {
-        // console.log(`swapping ${a} ${b} ${files[a].entity} with ${files[b].entity}`);
+        // console.log(`swapping ${a} ${b} ${files[a].path}: ${files[a].entity} with ${files[b].path}: ${files[b].entity}`);
         // console.log(files.map(file => file.entity));
         const bObj = files.splice(b, 1)[0];
         files.splice(a, 0, bObj);
@@ -134,7 +163,6 @@ while (changed) {
     }
   }
 }
-console.log('changeCounter', changeCounter)
 for (const file of files) {
   const thisChangeTime = fs.statSync(file.path).mtimeMs;
   if (thisChangeTime > lastCompile || argv[3] == '--build-all') {
